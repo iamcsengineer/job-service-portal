1c767c6d6d2bc74c6676ec1b43278d3fc73d9368
AnirudhGoutam2000 <0206cs181021@ggits.net>
2025-07-26 00:46:32 +0530

    Adding External API and DB Logic

--- src/main/java/com/microservice/job/service/portal/repository/RapidAPIRecordRepository.java
+++ src/main/java/com/microservice/job/service/portal/repository/RapidAPIRecordRepository.java
@@ -1,11 +1,16 @@
 package com.microservice.job.service.portal.repository;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.microservice.job.service.portal.entities.RapidAPIRecord;
 
 @Repository
-public interface RapidAPIRecordRepository extends JpaRepository<RapidAPIRecord, Integer>{
+public interface RapidAPIRecordRepository extends JpaRepository<RapidAPIRecord, Integer> {
 
+	@Query(value = "SELECT * FROM rapidapirecord ORDER BY id DESC LIMIT 1", nativeQuery = true)
+	RapidAPIRecord retrieveLastRecordTime();
 }
